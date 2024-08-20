2a310e33e64a9b94e366bd2c9f8570d1014fe01c
lts20050703 <lts20050703@gmail.com>
2024-08-21 01:33:50 +0700

    Switch to pnpm

--- eslint.config.js
+++ eslint.config.js
@@ -3,31 +3,14 @@ import ts from "typescript-eslint"
 import svelte from "eslint-plugin-svelte"
 import prettier from "eslint-config-prettier"
 import globals from "globals"
-
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs["flat/recommended"],
 	prettier,
 	...svelte.configs["flat/prettier"],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		files: ["**/*.svelte"],
-		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
-		}
-	},
-	{
-		ignores: ["build/", ".svelte-kit/", "dist/"]
-	}
+	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	{ files: ["**/*.svelte"], languageOptions: { parserOptions: { parser: ts.parser } } },
+	{ ignores: ["build/", ".svelte-kit/", "dist/"] }
 ]
